e116626263d1e3f11c78cf7e727e35930fd5a6c0
Tara Furstenau <tarafurstenau@Taras-MacBook-Pro.local>
2024-01-07 15:20:19 -0700

    update settings

--- lepto_lims/prod_settings.py
+++ lepto_lims/prod_settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
