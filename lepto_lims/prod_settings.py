42ea4b75047b74516613814bb11a0411c72b0598
Tara Furstenau <tarafurstenau@Taras-MacBook-Pro.local>
2024-01-07 12:58:02 -0700

    update settings

--- lepto_lims/prod_settings.py
+++ lepto_lims/prod_settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.lepto.outonalims.app', 'lepto.outonalims.app']
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 
 # Application definition
